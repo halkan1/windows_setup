e073f1a540e302ef5cfa9337258a069e93b33c72
halkan1 <lillhalkan@gmail.com>
2019-08-30 12:05:02 +0200

    modified ps app script

--- powershell_files/download_apps.ps1
+++ powershell_files/download_apps.ps1
@@ -1,3 +1,7 @@
 # Download Apps. Run this as your own user
 Invoke-WebRequest -Uri https://atom.io/download/windows_x64 -OutFile $env:USERPROFILE\Downloads\AtomSetup-x64.exe -UseBasicParsing
 Invoke-WebRequest -Uri https://laptop-updates.brave.com/latest/winx64 -OutFile $env:USERPROFILE\Downloads\BraveBrowserSetup.exe -UseBasicParsing
+
+# Install Atom Packages and settings
+# $env:USERPROFILE\AppData\Local\atom\bin\apm list --packages-file $env:USERPROFILE\Documents\development\general\atom_packages.txt
+# Windows Atom settings are stored in $env:USERPROFILE\.atom
