f18884e48347dd3101557d2f84a8e4fddf947fd0
halkan1 <lillhalkan@gmail.com>
2019-08-30 12:52:15 +0200

    restruct

--- powershell_files/create_dev_dirs.ps1
+++ powershell_files/create_dev_dirs.ps1
@@ -1,3 +1,23 @@
+function NewDirs {
+    [CmdletBinding()]
+    Param(
+        [Parameter(Mandatory = $True)]
+        [String] $Directory)
+
+    if (-not (Test-Path -LiteralPath $Directory)) {
+        try {
+            New-Item -Path $Directory -ItemType Directory -ErrorAction Stop | Out-Null #-Force
+        }
+        catch {
+            Write-Error -Message "Unable to create directory '$Directory'. Error was: $_" -ErrorAction Stop
+        }
+        "Successfully created directory '$Directory'."
+    }
+    else {
+        "Directory already existed"
+    }
+}
+
 $dir_array='python','wsl','general','windows','web','fonts','c'
 
-$dir_array | foreach { .\NewDir.ps1 -Directory $env:USERPROFILE\Documents\development\$_ }
+$dir_array | foreach { NewDirs -Directory $env:USERPROFILE\Documents\development1\$_ }
